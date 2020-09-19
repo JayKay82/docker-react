ac48d71b2d8c83b80f4ee02c056ed46d731ae56b
Jan Kroezen <jan.kroezen@bedsandbars.com>
2020-09-19 19:32:47 +0100

    update homepage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there again!
+          New AWS Deploy!
         </p>
         <a
           className="App-link"
